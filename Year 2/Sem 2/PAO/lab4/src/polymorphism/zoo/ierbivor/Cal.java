7e60a2152124188f6006bc3c72f4013d927ee70a
Radu Dilirici <diliriciradu@gmail.com>
2020-03-17 19:57:39 +0200

    Added 4th laboratory

---
+++ Year 2/Sem 2/PAO/lab4/src/polymorphism/zoo/ierbivor/Cal.java
@@ -0,0 +1,14 @@
+package polymorphism.zoo.ierbivor;
+
+public class Cal extends Ierbivor {
+
+    public Cal(String nume, int varsta) {
+        super(nume, varsta);
+        this.sunetSpecific = "necheaza";
+    }
+
+    @Override
+    public void scoateSunet() {
+        System.out.println("calul " + this.sunetSpecific);
+    }
+}
---
+++ Year 2/Sem 2/PAO/lab4/src/polymorphism/zoo/omnivor/Urs.java
@@ -0,0 +1,14 @@
+package polymorphism.zoo.omnivor;
+
+public class Urs extends Omnivor {
+
+    public Urs(String nume, int varsta) {
+        super(nume, varsta);
+        this.sunetSpecific = "mormaie";
+    }
+
+    @Override
+    public void scoateSunet() {
+        System.out.println("ursul " + this.sunetSpecific);
+    }
+}
